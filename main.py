4a72581d1e77e69686631e866972f18069d053bb
avinashzaware2025 <avinashzaware2025@gmail.com>
2025-04-22 13:50:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from quotexapi.stable_api import Quotex
+import os
+from dotenv import load_dotenv
 
-q = Quotex(email="truptiauti2001@gmail.com", password="Samarth@123")
+load_dotenv()
+
+email = os.getenv("QX_EMAIL")
+password = os.getenv("QX_PASSWORD")
+
+q = Quotex(email=email, password=password)
 
 if q.connect():
     print("✅ Login Successful")
